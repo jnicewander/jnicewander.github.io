62e9772238a18c0c00cd1739071fb470bcb01cf9
jnicewander <jnicewander@live.com>
2019-12-28 20:20:15 -0500

    more navbar work

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function navFunction() {
+    //add smooth scrolling class to html on base.scss
     let x = document.getElementById("topNav");
     if (x.className === "nav") {
         x.className += " responsive";
     } else {
         x.className = "nav";
     }
+}
+
+function closeMenu() {
+    let y = document.getElementById("topNav");
+    y.classList.remove("responsive");
 }
